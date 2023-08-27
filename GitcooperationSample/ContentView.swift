034463599ee9877d7ff433fe10d6bd2c17358f95
Koichiro Uchida <sk8lovedecember@icloud.com>
2023-08-27 11:55:55 +1000

    Initial Commit

---
+++ GitcooperationSample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitcooperationSample
+//
+//  Created by 内田幸一郎 on 27/8/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitcooperationSample/GitcooperationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitcooperationSampleApp.swift
+//  GitcooperationSample
+//
+//  Created by 内田幸一郎 on 27/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitcooperationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
